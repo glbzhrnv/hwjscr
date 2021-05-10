e3bbcf31ec1cd5ee23c0cd1f82dd745a1dd74ecf
Gleb <glebzhrnv13@mail.ru>
2021-05-10 17:51:54 +0300

    homework

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// task-1
+// const cube = (n) => {
+//     const findPower = (power) => {
+//       if (3 ** power > n) {
+//         return false;
+//       } else if (3 ** power < n) {
+//         return findPower(power + 1);
+//       }
+//       return true;
+//     };
+//     return findPower(0);
+//   };
+//   console.log(cube(9))
+
+// task-3
+// const fizzBuzz = (begin, end) => {
+//   if (begin > end) return;
+//   let result = begin;
+
+//   if (begin % 3 === 0 && begin % 5 === 0) {
+//     result = 'FizzBuzz';
+//   } else if (begin % 3 === 0) {
+//     result = 'Fizz';
+//   } else if (begin % 5 === 0) {
+//     result = 'Buzz';
+//   };
+	
+// 	console.log(result);
+
+//   fizzBuzz(begin + 1, end);
+
+// };
+// console.log(fizzBuzz(6,10))
+
